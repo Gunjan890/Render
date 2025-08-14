84a8a3f98c2782cf67f0662e730db23264158a0c
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2025-08-14 23:35:59 +0530

    Update __main__.py

--- IstkharMusic/__main__.py
+++ IstkharMusic/__main__.py
@@ -12,6 +12,11 @@ from IstkharMusic.plugins import ALL_MODULES
 from IstkharMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
+# Import and start keep-alive server
+from keep_alive import keep_alive
+
+
+
 
 async def init():
     if (
@@ -59,4 +64,5 @@ async def init():
 
 
 if __name__ == "__main__":
+    keep_alive()  # Start the web server for Render
     asyncio.get_event_loop().run_until_complete(init())
